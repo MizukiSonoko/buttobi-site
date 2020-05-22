033b741c7bf1981896f404dab832853b75750ed5
Sonoko Mizuki <sonoko@mizuki.io>
2020-05-22 21:30:54 +0900

    fix favicon path

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/u25challenge/favicon.ico' }
     ]
   },
   /*
