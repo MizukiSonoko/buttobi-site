a10f9cb1c094a4ebf8f6208f3fcd8f27da9930dd
Sonoko Mizuki <sonoko@mizuki.io>
2020-05-26 14:17:51 +0900

    update search box

--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,23 @@
   <v-layout
     column
     justify-center
-    align-center
   >
-    <v-flex
-      xs12
-      sm8
-      md6
+    <v-row
+        justify="center"
+        class="header"
+        align-content="center"
     >
-        <v-col cols="12">
-          <v-row
-            :align="alignment"
-            :justify="justify"
-          >
-            <v-text-field :rules="rules"></v-text-field>
-            <v-btn color="primary" class="my-button">Go travel</v-btn>
-          </v-row>
-        </v-col>
-    </v-flex>
+      <v-col class="d-flex" cols="6">
+        <v-select
+          :items="items"
+          label="Air Port"
+          solo
+        ></v-select>
+      </v-col>
+      <v-col class="d-flex" cols="4">
+        <v-btn color="primary" class="my-button">Go travel</v-btn>
+      </v-col>
+    </v-row>
   </v-layout>
 </template>
 
@@ -26,18 +26,17 @@
 import Vue from "vue";
 export default Vue.extend({
   data: () => ({
-    rules: [
-      (value: string) => !!value || 'Required.',
-      (value: string) => {
-        const supported: Array<String> = ["HND"]
-        return supported.includes(value) || '('+value+')対応していない空港です'
-      },
-    ],
+    items: ['HND', 'ITM'],
   }),
 });
 </script>
 
 <style lang="postcss" scoped>
-.my-button{
+.header{
+  min-height: 300px;
 }
+.my-button{
+  margin: 6px 0px;
+}
+
 </style>
