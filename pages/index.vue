74dbbe6a3c5ae682f6b86ca33645af16939af52f
Sonoko Mizuki <sonoko@mizuki.io>
2020-06-06 11:19:52 +0900

    Remove comments

--- pages/index.vue
+++ pages/index.vue
@@ -74,12 +74,9 @@ export default Vue.extend({
     changeOriginAirport: function () {
       const newSchedules = this.getAirplainsFrom(this.originAirport.value);
       this.data.splice(newSchedules.length)
-      console.log(newSchedules);
       Array.prototype.forEach.call(newSchedules, (s: any,i: number) => {
         Vue.set(this.data, i, s)
       })
-      console.log(this.$data.data);
-      console.log(this.originAirport.value, this.getAirplainsFrom(this.originAirport.value));
     },
     getAirplainsFrom: function (iata: string):any[] {
       const now = Date.now();
