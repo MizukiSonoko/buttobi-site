5fb3655947cd260fef99b3e78b0ce8d0b7299126
Sonoko Mizuki <sonoko@mizuki.io>
2020-06-12 11:51:21 +0900

    Add base css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-app>
-    <nuxt />
+  <v-app id="app">
+    <nuxt id="content" />
     <v-footer
-      :fixed="fixed"
+      fixed
       app
-      class="footer"
+      id="footer"
     >
       <span>&copy; {{ new Date().getFullYear() }} MizukiSonoko</span>
     </v-footer>
@@ -20,11 +20,35 @@ export default {
 </script>
 
 
-<style lang="postcss" scoped>
-#__nuxt{
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 8.4pt;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  height: 100%;
+}
+
+#__nuxt {
+  height: 100%;
+}
+
+#__layout {
+  height: 100%;
+}
+
+body {
+  height: 100%;
   background: #005b9f;
 }
-.footer{
+
+#footer{
+  width: 100%;
   background: #005b9f;
   color: #fff;
 }
